48a4bb5fefdef893e1e84371b71a6f83c9a39f5e
GreenALan <13804605210@163.com>
2016-11-21 21:54:57 +0800

    gogndo

---
+++ ExpressionTest1.java
@@ -0,0 +1,20 @@
+package computing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class ExpressionTest {
+	Expression testExpression;
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+       testExpression.main(null);
+	}
+
+}
